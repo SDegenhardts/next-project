e0b11d3f273be7bb35e2222866e1c8f4233dfcd6
Santhiago Degenhardt <santhiago.dg@hotmail.com>
2023-05-16 16:49:00 -0300

    .

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c5d370fd22f18182"; }
+/******/ 		__webpack_require__.h = function() { return "6033d8c6ff31ce74"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/login.js
+++ src/pages/login.js
@@ -4,6 +4,9 @@ export default function Login() {
   return (
     <section className={styles.container}>
       <div>
+        <h1 className={styles.font}>
+          SIGN IN
+        </h1>
         <input placeholder='Username or Email'></input>
         <input placeholder='Password'></input>
         <button><a href='/'>To Start</a></button>
