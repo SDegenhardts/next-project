3040abb093d4f25e301675b9b89391b65fbee2bc
Santhiago Degenhardt <santhiago.dg@hotmail.com>
2023-06-29 14:15:35 -0300

    add validation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d61fafc0e96e0303"; }
+/******/ 		__webpack_require__.h = function() { return "0d91da030f8b68fd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
